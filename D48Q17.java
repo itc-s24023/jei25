9b6c3dde9c4e905bb03298e82c1aaf8ca6b0aca7
itc-s24023 <s24023@std.it-college.ac.jp>
2025-05-16 11:40:37 +0900

    練習

---
+++ D48Q17.java
@@ -0,0 +1,13 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(n > 15 && n <= 30);
+    }
+}
+
+
+
+
+/*
+*D47は第47回を表しています
+* */
