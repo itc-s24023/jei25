8788e7ef605d1887596bbea8550db58dd8f6dc3c
itc-s24023 <s24023@std.it-college.ac.jp>
2025-05-30 13:00:06 +0900

    課題

---
+++ D50Q27.java
@@ -0,0 +1,18 @@
+public class D50Q27 {
+    public static void main(String[] args) {
+        int control = 3;
+        String mode;
+        switch (control) {
+            case 1:
+                mode = "Warm";
+                break;
+            case 2:
+                mode = "Cool";
+                break;
+            default:
+                mode = "Wind";
+                break;
+        }
+        System.out.println(mode);
+    }
+}
