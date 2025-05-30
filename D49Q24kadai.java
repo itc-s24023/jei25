9755daf35b952fbee2d719fceeb978b8dab6952a
itc-s24023 <s24023@std.it-college.ac.jp>
2025-05-30 10:01:29 +0900

    課題

---
+++ D49Q24kadai.java
@@ -0,0 +1,13 @@
+public class D49Q24kadai {
+    public static void main(String[] args) {
+
+        int values[] = {10,20,30,40};
+        int sum = 0;
+        for(int i=0; i<values.length; i++){
+            sum += values[i];
+        }
+
+        System.out.println(sum);
+        System.out.println(values.length);
+    }
+}
