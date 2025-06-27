72fa60a33207df20a228b3716b06087d392f4226
itc-s24023 <s24023@std.it-college.ac.jp>
2025-06-27 11:27:28 +0900

    課題

---
+++ D51Q20kadai.java
@@ -0,0 +1,12 @@
+public class D51Q20kadai {
+    public static void main(String[] args) {
+        int score = 85;
+        String result = (score >= 70) ? "合格" : "不合格";
+        System.out.println(result);
+    }
+}
+
+/*変数score = 85を設定し、条件演算(? :)を使って
+score = 70以上なら合格、未満なら不合格と表示されるコードを作成
+ */
+
