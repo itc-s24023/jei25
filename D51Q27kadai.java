07d383c408caee6d857554b1ce6fad9fc7331c68
itc-s24023 <s24023@std.it-college.ac.jp>
2025-07-04 12:40:12 +0900

    課題

---
+++ D51Q27kadai.java
@@ -0,0 +1,13 @@
+public class D51Q27kadai {
+    public static  String RED = "赤";
+    public static  String YELLOW = "黄";
+    public static  String BLUE = "青";
+
+    public static void main(String[] args) {
+        String currentLight = BLUE;
+
+
+        System.out.println("現在の信号は：" + currentLight);
+    }
+
+}
---
+++ D51Q5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class D51Q5 {
+    public static void main(String[] args) {
+       double [] level  = {0.0, 24.0, 57.0, Double.MAX_VALUE};
+       double [] unit = {0.0, 2.0, 3.0, 5.0};
+       int [] rate = {0, 19, 17, 14};
+       if(args.length != 1){
+           System.out.println("引数の個数が不正");
+           return;
+       }
+    }
+}
